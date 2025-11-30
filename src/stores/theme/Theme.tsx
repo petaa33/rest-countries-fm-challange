4610a59857e2dd7eff8d8db06bf77801533050ac
petaa33 <101650825+petaa33@users.noreply.github.com>
2025-11-30 22:55:34 +0100

    add theme context

---
+++ src/stores/theme/Theme.tsx
@@ -0,0 +1,47 @@
+import { createContext, useState } from "react";
+
+const enum ThemeEnum {
+    LIGHT,
+    DARK
+}
+
+const themes = new Map<ThemeEnum, string>([
+    [ThemeEnum.LIGHT, "theme-light"],
+    [ThemeEnum.DARK, "theme-dark"]
+]);
+
+interface Props {
+    children: React.ReactNode;
+}
+
+export interface IThemeContext {
+    theme: string,
+    handleToggle: () => void
+}
+
+export const ThemeContext = createContext<IThemeContext>({
+    theme: "light",
+    handleToggle: () => {}
+});
+
+const Theme: React.FC<Props> = ({children}) => {
+    const[theme, setTheme] = useState<string>(themes.get(ThemeEnum.DARK)!);
+
+    const handleToggle = () => {
+        setTheme(theme === themes.get(ThemeEnum.DARK) ? themes.get(ThemeEnum.LIGHT)! : themes.get(ThemeEnum.DARK)!);
+    }
+
+    return (
+        <ThemeContext value={{
+            theme,
+            handleToggle
+        }}>
+            {children}
+        </ThemeContext>
+    )
+}
+
+export default Theme;
+
+
+
