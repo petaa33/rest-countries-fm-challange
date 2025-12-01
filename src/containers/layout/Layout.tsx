12d76475f48404ee5b4b6d9a03810c476c52764f
petaa33 <101650825+petaa33@users.noreply.github.com>
2025-12-01 21:14:55 +0100

    update layout

--- src/containers/layout/Layout.tsx
+++ src/containers/layout/Layout.tsx
@@ -2,20 +2,17 @@ import React from 'react'
 import Header from '../../components/header/Header';
 import { useContext } from 'react';
 import {IThemeContext, ThemeContext} from '../../stores/theme/Theme';
+import { Outlet } from 'react-router';
 import style from "./layout.module.css"
 import "../../global.css"
 
-interface Props {
-    children: React.ReactNode;
-}
-
-const Layout: React.FC<Props> = ({children}) => {
+const Layout: React.FC = () => {
   const {handleToggle, theme} = useContext<IThemeContext>(ThemeContext);
 
   return (
     <div className={`${style.container} ${theme}`}>
         <Header />
-        {children}
+        <Outlet />
     </div>
   )
 }
